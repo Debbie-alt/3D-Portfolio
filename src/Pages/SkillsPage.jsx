c235f89886a26fef81d65979cd43f2a387c8c689
Debbie-alt <115513115+Debbie-alt@users.noreply.github.com>
2025-03-21 16:16:39 +0100

    add icons to skills page

--- src/Pages/SkillsPage.jsx
+++ src/Pages/SkillsPage.jsx
@@ -6,9 +6,13 @@ import js from "../assets/js_rounded.png";
 import css from "../assets/css_icon.png";
 import git from "../assets/github.png";
 import framer from "../assets/framer_motion.png";
-// import docker from "../assets/docker.png";
-// import react from "../assets/react.png";
-// import next from "../assets/nextjs.png";
+import reactv from "../assets/icons8-react.gif";
+import react from "../assets/react.png";
+import next from "../assets/next.png";
+import rust from "../assets/icons8-rust-logo.gif"
+import solana from "../assets/solana.png"
+import tailwind from '../assets/tailwind.png'
+import angular from '../assets/angular.png'
 
 const floatingAnimation = {
   y: [0, -10, 10, -5, 0], // Moves up/down
@@ -21,7 +25,7 @@ const floatingAnimation = {
 };
 
 const SkillsPage = () => {
-  const skills = [js, css, git, sql, framer,  bootstrap];
+  const skills = [js, css, git, solana, rust, react, next, framer, tailwind, angular];
 
   return (
     <div className="grid grid-cols-4 items-center justify-center gap-5 p-16 h-screen w-screen">
